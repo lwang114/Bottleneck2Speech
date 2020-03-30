9c737e26bfc219f0828b8790c56bb4bf9c329ae4
lwang114 <lwang114@illinois.edu>
2020-03-29 22:00:59 -0500

    scripted Babel

--- babel_prepare.py
+++ babel_prepare.py
@@ -14,9 +14,15 @@ class BabelKaldiPreparer:
     self.data_root = data_root
     self.sph2pipe = sph2pipe
 
+    '''
     self.transcripts = {'train': os.listdir(data_root+'conversational/training/transcript_roman/'),
                         'test': os.listdir(data_root+'conversational/eval/transcript_roman/'),
                         'dev':  os.listdir(data_root+'conversational/dev/transcript_roman/')} 
+    '''
+    self.transcripts = {'train': os.listdir(data_root+'scripted/training/transcript_roman/'),
+                        'test': os.listdir(data_root+'scripted/training/transcript_roman/'),
+                        'dev':  os.listdir(data_root+'scripted/training/transcript_roman/')} 
+
 
   def prepare_tts(self):
     if not os.path.isdir('data'):
@@ -24,11 +30,19 @@ class BabelKaldiPreparer:
       os.mkdir('data/train')
       os.mkdir('data/test')
       os.mkdir('data/dev')
-    
+   
+    '''
     sph_dir = {
       'train': self.data_root + 'conversational/training/',
       'test': self.data_root + 'conversational/eval/',
       'dev':  self.data_root + 'conversational/dev/'
+    }
+    '''
+    
+    sph_dir = {
+      'train': self.data_root + 'scripted/training/',
+      'test': self.data_root + 'scripted/training/',
+      'dev':  self.data_root + 'scripted/training/'
     }
       
     for x in ['train', 'dev', 'test']:
@@ -39,7 +53,12 @@ class BabelKaldiPreparer:
         wav_scp_f.truncate()
         utt2spk_f.truncate()
 
+        i = 0
         for transcript_fn in sorted(self.transcripts[x], key=lambda x:x.split('.')[0]):
+          # XXX
+          # if i > 1:
+          #   continue
+          i += 1
           utt_id = transcript_fn.split('.')[0]
           print(x, utt_id)
           sent = []
@@ -50,6 +69,9 @@ class BabelKaldiPreparer:
               words = line.strip().split(' ')             
               words = [w for w in words if w not in UNK and (w[0] != '<' or w[-1] != '>')]
               sent += words
+            if len(words) == 0:
+              print('Empty transcript file')
+              continue
 
           text_f.write(utt_id + ' ' + ' '.join(sent) + '\n')
           wav_scp_f.write(utt_id + ' ' + self.sph2pipe + ' -f wav -p -c 1 ' + \
