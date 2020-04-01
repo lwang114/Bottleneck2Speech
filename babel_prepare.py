003560b7b31f99fb30a6f282f4be7676fc5a6f50
Liming Wang <lwang114@illinois.edu>
2020-04-01 14:55:25 -0500

    fix utt2spk

--- babel_prepare.py
+++ babel_prepare.py
@@ -98,9 +98,8 @@ class BabelKaldiPreparer:
                     print('Empty segment')
                   continue
                 segment_f.write('%s_%04d %s %.1f %.1f\n' % (utt_id, i_seg, utt_id, start_sec, end_sec)) 
-                text_f.write('%s_%04d %s\n' % (utt_id, i_seg, ' '.join(words)))
-                
-            utt2spk_f.write('%s %s\n' % (utt_id, '001')) # XXX dummy speaker id
+                text_f.write('%s_%04d %s\n' % (utt_id, i_seg, ' '.join(words)))            
+                utt2spk_f.write('%s_%04d %s\n' % (utt_id, i_seg, '001')) # XXX dummy speaker id
             wav_scp_f.write(utt_id + ' ' + self.sph2pipe + ' -f wav -p -c 1 ' + \
                     os.path.join(sph_dir[x], 'audio/', utt_id + '.sph') + ' |\n')
           else:
