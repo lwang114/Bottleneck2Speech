9e1773e8d777f2835cd30d4c0fcb050447706c8d
Liming Wang <lwang114@illinois.edu>
2020-04-10 16:35:36 -0500

    finish VAD and remove silence

--- babel_prepare.py
+++ babel_prepare.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import librosa
+from copy import deepcopy
 
 UNK = ['(())']
 NONWORD = '~'
@@ -101,12 +102,14 @@ class BabelKaldiPreparer:
   def __init__(self, data_root, exp_root, sph2pipe, configs):
     self.audio_type = configs.get('audio_type', 'scripted')
     self.is_segment = configs.get('is_segment', False)
+    self.nonsilence_interval_file = configs.get('nonsilence_interval_file', 'nonsilence_intervals')
     self.vad = configs.get('vad', True)
     self.verbose = configs.get('verbose', 0)
     self.fs = 8000
     self.data_root = data_root
     self.exp_root = exp_root
     self.sph2pipe = sph2pipe
+    self.nonsilence_interval_files = {x: os.path.join('data', x, self.nonsilence_interval_file) for x in ['train', 'test', 'dev']}
 
     if self.audio_type == 'conversational':
       # XXX Use sub-train
@@ -158,7 +161,9 @@ class BabelKaldiPreparer:
       with open(os.path.join('data', x, 'text'), 'w') as text_f, \
            open(os.path.join('data', x, 'wav.scp'), 'w') as wav_scp_f, \
            open(os.path.join('data', x, 'utt2spk'), 'w') as utt2spk_f, \
-           open(os.path.join('data', x, 'segments'), 'w') as segment_f:  
+           open(os.path.join('data', x, 'segments'), 'w') as segment_f, \
+           open(os.path.join('data', x, 'nonsilence_intervals'), 'w') as nonsil_f:
+  
         text_f.truncate()
         wav_scp_f.truncate()
         utt2spk_f.truncate()
@@ -167,9 +172,9 @@ class BabelKaldiPreparer:
         i = 0
         for transcript_fn, audio_fn in zip(sorted(self.transcripts[x], key=lambda x:x.split('.')[0]), sorted(self.audios[x], key=lambda x:x.split('.')[0])):
           # XXX
-          # if i > 1:
-          #   continue
-          # i += 1
+          if i > 1:
+            continue
+          i += 1
 
           # Load audio
           os.system('%s -f wav -p -c 1 %s temp.wav' % (self.sph2pipe, sph_dir[x] + 'audio/' + audio_fn))
@@ -178,15 +183,14 @@ class BabelKaldiPreparer:
           utt_id = transcript_fn.split('.')[0]
           sent = []
           if self.is_segment:
-            print(x, utt_id)
+            print(i, x, utt_id)
             with open(sph_dir[x] + 'transcript_roman/' + transcript_fn, 'r') as transcript_f:
               lines = transcript_f.readlines()
               i_seg = 0
               for i_seg, (start, segment, end) in enumerate(zip(lines[::2], lines[1::2], lines[2::2])):
                 # XXX
-                # if i_seg > 5:
-                #   continue
-                # i_seg += 1
+                if i_seg > 5:
+                  continue
 
                 start_sec = float(start[1:-2])
                 end_sec = float(end[1:-2])
@@ -219,10 +223,11 @@ class BabelKaldiPreparer:
                 if self.vad:
                   start_sec_nonsils, end_sec_nonsils = VAD2(np.append(np.zeros((start,)), y[start:end]), self.fs)
                   if len(start_sec_nonsils) == 0:
-                    print('Audio too quiet')
-                    continue
-                  # for i_subseg, (start_sec_nonsil, end_sec_nonsil) in enumerate(zip(start_sec_nonsils, end_sec_nonsils)): 
+                    print('Audio too quiet: ', utt_id)
+                    continue 
                   segment_f.write('%s_%04d %s %.2f %.2f\n' % (utt_id, i_seg, utt_id, start_sec_nonsils[0], end_sec_nonsils[-1])) 
+                  for i_subseg, (start_sec_nonsil, end_sec_nonsil) in enumerate(zip(start_sec_nonsils, end_sec_nonsils)):
+                    nonsil_f.write('%s_%04d_%02d %s %.2f %.2f\n' % (utt_id, i_seg, i_subseg, utt_id, start_sec_nonsils[i_subseg], end_sec_nonsils[i_subseg]))
                 else:
                   segment_f.write('%s_%04d %s %.2f %.2f\n' % (utt_id, i_seg, utt_id, start_sec, end_sec)) 
 
@@ -250,25 +255,127 @@ class BabelKaldiPreparer:
             text_f.write(utt_id + ' ' + ' '.join(sent) + '\n')
             wav_scp_f.write(utt_id + ' ' + self.sph2pipe + ' -f wav -p -c 1 ' + \
                 os.path.join(sph_dir[x], 'audio/', utt_id + '.sph') + ' |\n')
-            utt2spk_f.write(utt_id + ' ' + '001\n') # XXX dummy speaker id
+            utt2spk_f.write(utt_id + ' ' + utt_id) # XXX dummy speaker id
       if not self.is_segment:
         os.remove(os.path.join('data', x, 'segments'))
   
-  # TODO
-  def remove_silence(self):
-    pass
-    # feat_dir = 'fbank/'
-    # if not os.path.isdir('fbank_no_silence/'):
-    #   os.mkdir('fbank_no_silence')
-    # ark_files = {
-    #   'train': os.listdir(feat_dir+'*_train.ark'),
-    #   'dev': os.listdir(feat_dir+'*_dev.ark'),
-    #   'test': os.listdir(feat_dir+'*_test.ark')
-    #  }
-    
-    # Detect silence interval
+  def remove_silence(self, out_dir='BABEL_BP_101_NONSIL/'):
+    if not os.path.isdir('data'):
+      os.mkdir('data')
 
-    # Skip silence interval and concatenate the nonsilence speech
+    if not os.path.isdir('data/train_nonsil'):
+      os.mkdir('data/train_nonsil')
+    if not os.path.isdir('data/test_nonsil'):
+      os.mkdir('data/test_nonsil')
+    if not os.path.isdir('data/dev_nonsil'):
+      os.mkdir('data/dev_nonsil')
+    
+    if not os.path.isdir(out_dir):
+      os.mkdir(out_dir)
+      os.mkdir(out_dir + 'train')
+      os.mkdir(out_dir + 'test')
+      os.mkdir(out_dir + 'dev')
+
+    if not os.path.isdir(out_dir + 'train'):
+      os.mkdir(out_dir + 'train')
+    if not os.path.isdir(out_dir + 'test'):
+      os.mkdir(out_dir + 'test')
+    if not os.path.isdir(out_dir + 'dev'):
+      os.mkdir(out_dir + 'dev')
+
+    if not os.path.isdir(self.exp_root):
+      os.mkdir(exp_root)
+      os.mkdir(exp_root + 'train_nonsil')
+      os.mkdir(exp_root + 'test_nonsil')
+      os.mkdir(exp_root + 'dev_nonsil')
+ 
+    if self.audio_type == 'conversational':    
+      sph_dir = {
+      'train': self.data_root + 'conversational/training/',
+      'test': self.data_root + 'conversational/eval/',
+      'dev':  self.data_root + 'conversational/dev/'
+      }
+    elif self.audio_type == 'scripted':
+      sph_dir = {
+      'train': self.data_root + 'scripted/training/',
+      'test': self.data_root + 'scripted/training/',
+      'dev':  self.data_root + 'scripted/training/'
+      }
+    else:
+      raise NotImplementedError
+    
+    nonsilence_intervals = {}
+
+    for x in ['train', 'dev', 'test']:
+      with open(self.nonsilence_interval_files[x], 'r') as nonsil_f:
+        for line in nonsil_f:
+          utt_id = line.split()[1]
+          seg_id = line.split()[0].split('_')[-2]
+          
+          if utt_id not in nonsilence_intervals:
+            nonsilence_intervals[utt_id] = {}
+
+          start_sec, end_sec = float(line.split()[-2]), float(line.split()[-1])
+          if seg_id in nonsilence_intervals:
+            nonsilence_intervals[utt_id][seg_id].append([start_sec, end_sec])
+          else:
+            nonsilence_intervals[utt_id][seg_id] = [[start_sec, end_sec]]
+  
+    for x in ['train', 'dev', 'test']:
+      # TODO Handle unsegmented utterances
+      if self.is_segment:
+        with open(os.path.join('data', x+'_nonsil', 'text'), 'w') as text_f, \
+             open(os.path.join('data', x+'_nonsil', 'wav.scp'), 'w') as wav_scp_f, \
+             open(os.path.join('data', x+'_nonsil', 'utt2spk'), 'w') as utt2spk_f, \
+             open(os.path.join('data', x+'_nonsil', 'segments'), 'w') as segment_f:  
+          text_f.truncate()
+          wav_scp_f.truncate()
+          utt2spk_f.truncate()
+          segment_f.truncate()
+
+          i = 0 
+          for transcript_fn, audio_fn in zip(sorted(self.transcripts[x], key=lambda x:x.split('.')[0]), sorted(self.audios[x], key=lambda x:x.split('.')[0])):
+            # XXX
+            if i > 1:
+              continue
+            i += 1
+            utt_id = transcript_fn.split('.')[0]
+            print(i, x, utt_id)
+
+            # Convert .SPH files into .wav
+            os.system('%s -f wav -p -c 1 %s temp.wav' % (self.sph2pipe, sph_dir[x] + 'audio/' + audio_fn))
+ 
+            # Load .wav file
+            y, _ = librosa.load('temp.wav', sr=self.fs)
+            y_nonsil = []
+
+            # Load segment intervals
+            nonsil_whole_utterance = nonsilence_intervals[utt_id]    
+            start_seg, end_seg = 0, 0
+            for i_seg, seg_id in enumerate(sorted(nonsil_whole_utterance, key=lambda x:int(x))):
+              nonsil_segment = nonsil_whole_utterance[seg_id]
+              # XXX
+              if i_seg > 5:
+                continue
+              print(seg_id) 
+
+              
+              for start_end in nonsil_segment:
+                start = int(start_end[0] * self.fs)
+                end = int(start_end[1] * self.fs) 
+                seg = y[start:end] 
+                y_nonsil.append(seg)
+                end_seg += (start_end[1] - start_end[0])  
+              
+              segment_f.write('%s_%04d %s %.2f %.2f\n' % (utt_id, i_seg, utt_id, start_seg, end_seg))
+              start_seg = end_seg
+
+            y_nonsil = np.concatenate(y_nonsil, axis=0)
+            print(y_nonsil.shape)
+
+            # Segment the audio, save it as a .wav file
+            wav_scp_f.write(utt_id + ' ' + os.path.join(out_dir, x, audio_fn) + '\n')
+            librosa.output.write_wav(os.path.join(out_dir, x, audio_fn.split('.')[0]+'.wav'), y_nonsil, sr=self.fs)
   
 if __name__ == '__main__':
   data_root = '/Users/liming/research/data/IARPA_BABEL_BP_101/'
@@ -279,3 +386,4 @@ if __name__ == '__main__':
   configs = {'audio_type': 'conversational', 'is_segment': True, 'vad': True}
   kaldi_prep = BabelKaldiPreparer(data_root, exp_root, sph2pipe, configs)
   kaldi_prep.prepare_tts()
+  kaldi_prep.remove_silence() 
